58457b7c05ab7ef5c819e4950f8d95fe6453055b
Kavya Malviya <83281151+Kavyamalviya6697@users.noreply.github.com>
2021-06-02 10:22:38 +0530

    Add files via upload

--- Sample.java
+++ Sample.java
@@ -31,30 +31,54 @@ class Mango extends Fruits {
 
 class Sample {
 	public static void main(String args[]) {
-		Fruits o1 = new Fruits();
-		o1.show();
-		o1.display();
-
-		Apple o2 = new Apple();
-		o2.show();
-		o2.display();
-
-		Fruits o3 = new Apple();
-		o3.show();
-		o3.display();
-
-		Apple o4 = new Apple();
-		Fruits o5 = o4;
-		o5.show();
-
 		Apple o6 = new Apple();
 		Fruits o7 = o6;
-		
-		if(o7 instanceof Mango) {
-			Mango o8 = (Mango) o7;
+
+		if(o7 instanceof Apple) {
+
+			System.out.println("Instance of Apple");
+			Apple o8 = (Apple) o7;
 			o8.show();
 			o8.display();
+			
 		}
+		
+
+		Mango o9 = new Mango();
+		Fruits o10 = o9;
+
+		if(o10 instanceof Mango) {
+			System.out.println("Instance of mango");
+			Mango o8 = (Mango) o10;
+			o8.show();
+			o8.display();
+			
+		}
+
+		// Fruits o1 = new Fruits();
+		// o1.show();
+		// o1.display();
+
+		// Apple o2 = new Apple();
+		// o2.show();
+		// o2.display();
+
+		// Fruits o3 = new Apple();
+		// o3.show();
+		// o3.display();
+
+
+
+		// Apple o4 = new Apple();
+		// Fruits o5 = o4;
+		// o5.show();
+		// o4.show();
+		// o5.display();
+		
+
+		
+		
+		
 
 	}
 }
